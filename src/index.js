22cf879575bfc12a0096cb403f4e5e5219e22f03
SARSENY <32071091+SARSENY@users.noreply.github.com>
2018-02-23 12:53:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,24 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var first,second,third,lovers=0,chislo=1;
+  preferences.unshift(0);
+  for (chislo;chislo<preferences.length;chislo++)
+  {
+    if (preferences[chislo]>0 && preferences[chislo]!=chislo)
+    {
+      first=chislo;
+      second=preferences[first];
+      third=preferences[second];
+      //console.log(chislo,'i',first,'f',second,'s',third,'t');
+      if (first==preferences[third])
+      {
+        lovers=lovers+1;
+        preferences[first]=0;
+        preferences[second]=0;
+        preferences[third]=0;
+      }
+    }
+  }
+  return lovers;
 };
